9a6acb24c8f0727dd0c664b84214a5be0a983237
DinaraKA <d.karybekova@gmail.com>
2019-09-25 11:22:46 +0600

    Added task view page, modified urls, views and style

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -9,3 +9,12 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
         return context
+
+class TaskView(TemplateView):
+    template_name = 'task.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task_pk = kwargs.get('pk')
+        context['task'] = get_object_or_404(Task, pk=task_pk)
+        return context
