899ffbf29249b2bf13a59c3cfdbf2d62a8c2f2e6
DinaraKA <d.karybekova@gmail.com>
2019-09-25 12:39:19 +0600

    Added task update page, modified urls, views, index.html, create.html and style.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -37,3 +37,27 @@ class TaskCreateView(View):
             return redirect('task_view', pk=task.pk)
         else:
             return render(request, 'create.html', context={'form': form})
+
+class TaskUpdateView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        form = TaskForm(data={
+            'summary':task.summary,
+            'description':task.description,
+            'status':task.status,
+            'type': task.type
+        })
+        return render(request, 'update.html', context={'form': form, 'task':task})
+
+    def post(self, request, pk):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = Task.objects.create(
+                summary=form.cleaned_data['summary'],
+                description=form.cleaned_data['description'],
+                status=form.cleaned_data['status'],
+                type=form.cleaned_data['type']
+            )
+            return redirect('task_view', pk=pk)
+        else:
+            return render(request, 'create.html', context={'form': form})
