73518b1f29f237efaba271fcc48e490eacff7aca
DinaraKA <d.karybekova@gmail.com>
2019-09-25 23:44:35 +0600

    Added task delete page, modified urls, views, index.html, task.html and style.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -58,6 +58,19 @@ class TaskUpdateView(View):
                 status=form.cleaned_data['status'],
                 type=form.cleaned_data['type']
             )
-            return redirect('task_view', pk=pk)
+            return redirect('task_view', pk=task.pk)
         else:
             return render(request, 'create.html', context={'form': form})
+
+class TaskDeleteView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        return render(request, 'delete.html', context={'task': task})
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return redirect('index')
+
+
+
