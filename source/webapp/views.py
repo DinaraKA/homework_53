87a8757f4461d96278fa0b9e000b6ef238a0db63
DinaraKA <d.karybekova@gmail.com>
2019-09-25 10:36:48 +0600

    Added main page index, modified urls and views, added style

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView
+from webapp.models import Task
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
