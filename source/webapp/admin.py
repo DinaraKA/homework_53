ec51f6b11ec238de6e156f2db947bcf93a3bc8e5
DinaraKA <d.karybekova@gmail.com>
2019-09-25 08:44:02 +0600

    Added models, regictered them in admin, made migration and dumped data.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Task, Status, Type
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'description', 'status', 'type', 'created_at']
+    list_filter = ['status', 'type']
+    list_display_links = ['pk', 'summary', 'description']
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    status_name = models.CharField(max_length=20, verbose_name='Status_name')
+
+    def __str__(self):
+        return self.status_name
+
+
+class Type(models.Model):
+    type_name = models.CharField(max_length=20, verbose_name='Type_name')
+
+    def __str__(self):
+        return self.type_name
+
+class Task(models.Model):
+    summary = models.CharField(max_length=100, verbose_name="Summary")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Description")
+    status = models.ForeignKey(Status, related_name='statuses', on_delete=models.PROTECT, verbose_name='Status')
+    type = models.ForeignKey(Type, related_name='types', on_delete=models.PROTECT, verbose_name='Type')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created date')
+
+    def __str__(self):
+        return self.summary
